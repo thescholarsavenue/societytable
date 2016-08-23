0061eb2426af19c0dc5fb5e539faf1f0c7feda2e
Ashris Choudhury <ashris.me@gmail.com>
2016-08-23 10:53:33 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,480 @@
+datax=[
+  {
+    "Index": 1,
+    "Symbol": "Os",
+    "Name": "Kharagpur Open Source Society",
+    "Year": 2013,
+    "Size": "XL",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/iitkgpfc/"
+  },
+  {
+    "Index": 2,
+    "Symbol": "Sp",
+    "Name": "Spectra",
+    "Year": 2007,
+    "Size": "M",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/Spectra.IITKGP/"
+  },
+  {
+    "Index": 3,
+    "Symbol": "Ag",
+    "Name": "Autonomous Ground vehicle Research Group",
+    "Year": 2007,
+    "Size": "NA",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/agvkgp/"
+  },
+  {
+    "Index": 4,
+    "Symbol": "Gt",
+    "Name": "The KGPian Game Theory Society",
+    "Year": 2010,
+    "Size": "M",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/The.KGTS/"
+  },
+  {
+    "Index": 5,
+    "Symbol": "Qu",
+    "Name": "Quiz Club",
+    "Year": 2003,
+    "Size": "S",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/iitkgp.quizclub/"
+  },
+  {
+    "Index": 6,
+    "Symbol": "Lg",
+    "Name": "KGP Language Learners’ Group",
+    "Year": 2015,
+    "Size": "S",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/groups/732492363547374/"
+  },
+  {
+    "Index": 7,
+    "Symbol": "Dr",
+    "Name": "Bengali Technology Dramatics Society (Druheen)",
+    "Year": 1970,
+    "Size": "XL",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/tdsdruheen/"
+  },
+  {
+    "Index": 8,
+    "Symbol": "Wt",
+    "Name": "Western Technology Music Group",
+    "Year": 2008,
+    "Size": "S",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/wtmsiitkgp/"
+  },
+  {
+    "Index": 9,
+    "Symbol": "Au",
+    "Name": "Autonomous Underwater Vehicle Research Group",
+    "Year": 2010,
+    "Size": "S",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/auviitkgp/"
+  },
+  {
+    "Index": 10,
+    "Symbol": "St",
+    "Name": "Space Technology Students’ Society (spAts)",
+    "Year": "NA",
+    "Size": "NA",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/spAce.iitkgp/"
+  },
+  {
+    "Index": 11,
+    "Symbol": "Bc",
+    "Name": "Business Club",
+    "Year": 2009,
+    "Size": "M",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/bclubkgp/"
+  },
+  {
+    "Index": 12,
+    "Symbol": "Li",
+    "Name": "Technology Literary Society",
+    "Year": 1950,
+    "Size": "M",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/tlsiitkgp/"
+  },
+  {
+    "Index": 13,
+    "Symbol": "Pr",
+    "Name": "Telugu technology Dramatics Society (Prasthanam)",
+    "Year": 2010,
+    "Size": "XL",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/Prasthanam.TDS/"
+  },
+  {
+    "Index": 14,
+    "Symbol": "Fp",
+    "Name": "Technology Filmmaking and Photography Society",
+    "Year": 2011,
+    "Size": "L",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/tfps.iitkgp/"
+  },
+  {
+    "Index": 15,
+    "Symbol": "Dt",
+    "Name": "Kharagpur Data Analytics Group",
+    "Year": 2014,
+    "Size": "S",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/kgpdag/"
+  },
+  {
+    "Index": 16,
+    "Symbol": "Rx",
+    "Name": "Technology Robotix  Society",
+    "Year": 2001,
+    "Size": "M",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/robotixiitkgp/"
+  },
+  {
+    "Index": 17,
+    "Symbol": "Fm",
+    "Name": "Food/Mess Monitoring Committee",
+    "Year": 2010,
+    "Size": "S",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/groups/402924043246682/"
+  },
+  {
+    "Index": 18,
+    "Symbol": "Ac",
+    "Name": "Alumni Cell",
+    "Year": 2008,
+    "Size": "L",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/iitkgp.alumnicell/"
+  },
+  {
+    "Index": 19,
+    "Symbol": "Tg",
+    "Name": "Technology Transfer Group",
+    "Year": 2007,
+    "Size": "L",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/TTG.SRIC/"
+  },
+  {
+    "Index": 20,
+    "Symbol": "Sf",
+    "Name": "Spring Fest",
+    "Year": 1960,
+    "Size": "XL",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/springfest.iitkgp/"
+  },
+  {
+    "Index": 21,
+    "Symbol": "Br",
+    "Name": "Branding and Relations Cell",
+    "Year": 2015,
+    "Size": "S",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/Branding-And-Relations-Cell-IIT-Kharagpur-123930134625611/"
+  },
+  {
+    "Index": 22,
+    "Symbol": "Fi",
+    "Name": "Finterest",
+    "Year": "NA",
+    "Size": "NA",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/Finterest/"
+  },
+  {
+    "Index": 23,
+    "Symbol": "Cm",
+    "Name": "Communiqué",
+    "Year": 2006,
+    "Size": "S",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/communique.iitkgp/"
+  },
+  {
+    "Index": 24,
+    "Symbol": "Aw",
+    "Name": "Awaaz",
+    "Year": 2006,
+    "Size": "L",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/awaaziitkgp/"
+  },
+  {
+    "Index": 25,
+    "Symbol": "Ph",
+    "Name": "Hindi Technology Dramatics Society (Pravaah)",
+    "Year": 1980,
+    "Size": "XL",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/Pravaah/"
+  },
+  {
+    "Index": 26,
+    "Symbol": "Bk",
+    "Name": "Technology Dance Society (Breakfree)",
+    "Year": "NA",
+    "Size": "L",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/Breakfreenowornever/"
+  },
+  {
+    "Index": 27,
+    "Symbol": "Tk",
+    "Name": "Team KART",
+    "Year": 2008,
+    "Size": "M",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/TeamKART/"
+  },
+  {
+    "Index": 28,
+    "Symbol": "Kg",
+    "Name": "Kharagpur Robosoccer Students’ Group",
+    "Year": 2008,
+    "Size": "L",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/krssg/"
+  },
+  {
+    "Index": 29,
+    "Symbol": "Cy",
+    "Name": "Child Rights and You KGP",
+    "Year": 2014,
+    "Size": "NA",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/CRY.IITKGP/"
+  },
+  {
+    "Index": 30,
+    "Symbol": "Gw",
+    "Name": "Gopali Youth Welfare Society",
+    "Year": "NA",
+    "Size": "NA",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/gyws.iitkgp/"
+  },
+  {
+    "Index": 31,
+    "Symbol": "Ni",
+    "Name": "NET Impact IIT Kharagpur",
+    "Year": 2015,
+    "Size": "XL",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/netimpactiitkharagpur/"
+  },
+  {
+    "Index": 32,
+    "Symbol": "Am",
+    "Name": "Ambar",
+    "Year": 2015,
+    "Size": "M",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/ambar.iitkgp/"
+  },
+  {
+    "Index": 33,
+    "Symbol": "Ch",
+    "Name": "Chess IITKGP",
+    "Year": 2015,
+    "Size": "XL",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/groups/chessiitkgp/"
+  },
+  {
+    "Index": 34,
+    "Symbol": "Co",
+    "Name": "Cognitio",
+    "Year": 2012,
+    "Size": "S",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/cognitioeducation/"
+  },
+  {
+    "Index": 35,
+    "Symbol": "Mk",
+    "Name": "MetaKGP",
+    "Year": 2015,
+    "Size": "XL",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/metakgp/"
+  },
+  {
+    "Index": 36,
+    "Symbol": "Pd",
+    "Name": "ProDex",
+    "Year": 2014,
+    "Size": "M",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/prodex.iitkgp/"
+  },
+  {
+    "Index": 37,
+    "Symbol": "As",
+    "Name": "Technology Adventure Society",
+    "Year": "NA",
+    "Size": "NA",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/TAdS.IIT/"
+  },
+  {
+    "Index": 38,
+    "Symbol": "Ai",
+    "Name": "AIESEC IITKGP",
+    "Year": 2010,
+    "Size": "L",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/aieseciitkgp/"
+  },
+  {
+    "Index": 39,
+    "Symbol": "Ec",
+    "Name": "Entrepreneurship Cell",
+    "Year": 2006,
+    "Size": "L",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/ecell.iitkgp/"
+  },
+  {
+    "Index": 40,
+    "Symbol": "Sg",
+    "Name": "Student Welfare Group",
+    "Year": 2011,
+    "Size": "M",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/SWGIITKGP/"
+  },
+  {
+    "Index": 41,
+    "Symbol": "Kj",
+    "Name": "Kshitij",
+    "Year": 2004,
+    "Size": "XL",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/ktj.iitkgp/"
+  },
+  {
+    "Index": 42,
+    "Symbol": "Ir",
+    "Name": "International Relations Cell",
+    "Year": 2015,
+    "Size": "S",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/IRCIITKGP/"
+  },
+  {
+    "Index": 43,
+    "Symbol": "Ts",
+    "Name": "The Street",
+    "Year": 2014,
+    "Size": "S",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/streetiitkgp/"
+  },
+  {
+    "Index": 44,
+    "Symbol": "Ds",
+    "Name": "Debating Society",
+    "Year": 2009,
+    "Size": "M",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/kgpdebsoc/"
+  },
+  {
+    "Index": 45,
+    "Symbol": "Sa",
+    "Name": "The Scholars' Avenue",
+    "Year": 2004,
+    "Size": "M",
+    "Recognized": 0,
+    "Link": "https://www.facebook.com/scholarsavenue/"
+  },
+  {
+    "Index": 46,
+    "Symbol": "En",
+    "Name": "English Technology Dramatics Society (Encore)",
+    "Year": 1980,
+    "Size": "XL",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/TDSEncore/"
+  },
+  {
+  	"Index":47,
+    "Symbol": "Et",
+    "Name": "Eastern Technology Music Society",
+    "Year": 2008,
+    "Size": "S",
+    "Recognized": 1,
+    "Link": "https://www.facebook.com/wtmsiitkgp/"
+  }
+];
+
+$(document).ready(function(){
+	var h=$('.element').css('width');
+$('.element').css("height",h);
+
+$('.element').html('<div class="topleft">A</div><div class="topright">B</div><div class="elename">Mg</div><div class="botleft">C</div><div class="botright">D</div>');
+$('.yes').attr("data-toggle","tooltip");
+// $('.yes').attr("data-delay",'{"show":"200", "hide":"200"}');
+
+
+$('.yes>.topleft').each(function(i){
+	i++;
+	if(i<=28) var q=i;
+	if(i>=38 && i<=47) var q=i-9;
+	if(i>=29 && i<=37) var q=i+9;
+	// if(i>=39 && i<=48) $(this).html(i-9);
+	$(this).html(q);
+	$(this).parent().attr("socindex",q);
+});
+$('.yes').each(function(i){
+	var ind=$(this).attr("socindex");
+	$(this).attr("title",datax[ind-1].Name);
+	$(this).click(function(){
+if(datax[ind-1].Link.indexOf('groups')==-1){window.open(datax[ind-1].Link+'about/?tab=page_info', '_blank');}
+else{window.open(datax[ind-1].Link, '_blank');}
+	});
+});
+
+
+$('.yes>.topright').each(function(i){
+	var ind=$(this).parent().attr("socindex");
+	$(this).html(datax[ind-1].Year);
+});
+$('.yes>.botleft').each(function(i){
+	var ind=$(this).parent().attr("socindex");
+	if(datax[ind-1].Recognized==1){$(this).html('*');}
+	else{$(this).html('');}
+});
+$('.yes>.botright').each(function(i){
+	var ind=$(this).parent().attr("socindex");
+	$(this).html(datax[ind-1].Size);
+
+});
+$('.yes>.elename').each(function(i){
+	var ind=$(this).parent().attr("socindex");
+	$(this).html(datax[ind-1].Symbol);
+
+});
+$('.donn').html('<div class="elename">&nbsp;&darr;</div>');
+$('[data-toggle="tooltip"]').tooltip(); 
+});
+$(document).resize(function(){
+	var h=$('.element').css('width');
+$('.element').css("height",h);
+});
